c42b54805a0f0401612c9ae9fb7bfef3cf3d2f53
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-06 16:54:15 +0100

    ADD > Main component

---
+++ src/components/Main.vue
@@ -0,0 +1,36 @@
+<script>
+import ProductCard from './ProductCard.vue';
+
+export default {
+  components: {
+    ProductCard,
+  },
+
+  data() {
+    return {
+      message: 'main',
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <main class="page-content">
+    <div class="container">
+      <div class="row">
+        <ProductCard v-for="n in 6"/>
+      </div>
+    </div>
+  </main>
+
+</template>
+
+<style lang="scss">
+
+.page-content {
+  padding: 40px 10px;
+}
+
+</style>
