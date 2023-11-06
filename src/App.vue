2fce9969a5a0f84dfe43cb4360e2d6ebcf551b3f
MicheleSerafini <serafini.michele01@gmail.com>
2023-11-06 15:01:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+  data() {
+    return {
+      message: 'hello vite'
+    }
+  }
+}
+
+</script>
+
+<template>
+  
+  <h2>{{ message }}</h2>
+
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+
+</style>
